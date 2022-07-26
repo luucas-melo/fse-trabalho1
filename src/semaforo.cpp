80489d33d7158ea89596c16ea5d41da47b8fe6a8
luucas-melo <lucasmelodos322@gmail.com>
2022-07-26 13:52:34 -0300

    feat: Adicionado lógica do semaforo da via auxiliar

--- src/semaforo.cpp
+++ src/semaforo.cpp
@@ -25,26 +25,41 @@ void start_semaforo()
 
     SemaforoVias semaforoVias;
     int numStates = 6;
+    // vermelho total
     semaforoVias.principal[0].active = SEMAFORO_1_VERMELHO(2);
-    semaforoVias.principal[0].minTime = 5000;
+    semaforoVias.auxiliar[0].active = SEMAFORO_2_VERMELHO(2);
+    semaforoVias.principal[0].minTime = 1000;
+
     semaforoVias.principal[1].active = SEMAFORO_1_VERDE(2);
+    semaforoVias.auxiliar[1].active = SEMAFORO_2_VERMELHO(2);
     semaforoVias.principal[1].minTime = 20000;
     semaforoVias.principal[2].active = SEMAFORO_1_AMARELO(2);
+    semaforoVias.auxiliar[2].active = SEMAFORO_2_VERMELHO(2);
     semaforoVias.principal[2].minTime = 3000;
+    // vermelho total
     semaforoVias.principal[3].active = SEMAFORO_1_VERMELHO(2);
-    semaforoVias.principal[3].minTime = 5000;
+    semaforoVias.auxiliar[3].active = SEMAFORO_2_VERMELHO(2);
+    semaforoVias.principal[3].minTime = 1000;
+
     semaforoVias.principal[4].active = SEMAFORO_1_VERMELHO(2);
+    semaforoVias.auxiliar[4].active = SEMAFORO_2_VERDE(2);
     semaforoVias.principal[4].minTime = 5000;
     semaforoVias.principal[5].active = SEMAFORO_1_VERMELHO(2);
+    semaforoVias.auxiliar[5].active = SEMAFORO_2_AMARELO(2);
     semaforoVias.principal[5].minTime = 5000;
     semaforoVias.principal[6].active = SEMAFORO_1_AMARELO(2);
+    semaforoVias.auxiliar[6].active = SEMAFORO_2_AMARELO(2);
     semaforoVias.principal[6].minTime = 3000;
 
     if (wiringPiSetup() == -1)
         exit(1);
     for (int i = 0; i < 6; i++)
     {
+
         pinMode(semaforoVias.principal[i].active, OUTPUT);
+        pinMode(semaforoVias.auxiliar[i].active, OUTPUT);
+        digitalWrite(semaforoVias.principal[i].active, LOW);
+        digitalWrite(semaforoVias.auxiliar[i].active, LOW);
     }
     int i = 0;
     while (1)
@@ -52,8 +67,10 @@ void start_semaforo()
         if (i > 6)
             i = 0;
         digitalWrite(semaforoVias.principal[i % numStates].active, HIGH);
+        digitalWrite(semaforoVias.auxiliar[i % numStates].active, HIGH);
         delay(semaforoVias.principal[i % numStates].minTime);
         digitalWrite(semaforoVias.principal[i % numStates].active, LOW);
+        digitalWrite(semaforoVias.auxiliar[i % numStates].active, LOW);
         printf("blink");
         i++;
     }
