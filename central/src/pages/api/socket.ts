2ca31df4b011caaaefe9dcf37b32f8eeb5591715
luucas-melo <lucasmelodos322@gmail.com>
2022-08-03 13:45:35 -0300

    Handle socket NIGHT_MODE event

--- central/src/pages/api/socket.ts
+++ central/src/pages/api/socket.ts
@@ -1,23 +1,23 @@
-import fs from 'fs';
-import net from 'net';
+import fs from 'fs'
+import net from 'net'
 
-import { NextApiRequest, NextApiResponse } from 'next';
-import { Server } from 'socket.io';
-import { DefaultEventsMap } from 'socket.io/dist/typed-events';
-import { messageConverter } from '../../utils/messageConverter';
+import { NextApiRequest, NextApiResponse } from 'next'
+import { Server } from 'socket.io'
+import { DefaultEventsMap } from 'socket.io/dist/typed-events'
+import { messageConverter } from '@/utils/messageConverter'
 
 interface ExtendedNextApiResponse<T = any> extends NextApiResponse<T> {
-  server: net.Server;
+  server: net.Server
   socket:
     | (net.Socket & {
         server: {
-          io: Server<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>;
-        };
+          io: Server<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>
+        }
       })
-    | null;
+    | null
 }
 
-const connectedSockets: Record<string, net.Socket> = {};
+const connectedSockets: Record<string, net.Socket> = {}
 
 const SocketHandler = (req: NextApiRequest, res: ExtendedNextApiResponse) => {
   //   console.log('RES', res?.socket?.server?.io);
@@ -25,95 +25,100 @@ const SocketHandler = (req: NextApiRequest, res: ExtendedNextApiResponse) => {
   switch (req.method) {
     case 'GET': {
       if (!res?.socket?.server?.io) {
-        console.log('Starting Socket.io');
-        const io = new Server(res?.socket?.server);
+        console.log('Starting Socket.io')
+        const io = new Server(res?.socket?.server)
+
+        // @ts-ignore
+        res.socket.server.io = io
 
         io.on('connection', (socket) => {
-          console.log('RODOU CONNECTION');
+          console.log('RODOU CONNECTION')
           socket.on('SEND_MESSAGE', (data: ServerMessage) => {
-            console.log('DATA', data);
-            const { host, type, message } = data;
+            console.log('DATA', data)
+            const { host, type, message } = data
 
             connectedSockets?.[host]?.write?.(
               JSON.stringify({
                 type,
-                message,
+                message
               })
-            );
-          });
-        });
+            )
+          })
+        })
 
-        // @ts-ignore
-        res.socket.server.io = io;
-
-        console.log(process.env.NODE_ENV);
-        const server = net.createServer();
+        console.log(process.env.NODE_ENV)
+        const server = net.createServer()
 
         server.on('error', (e) => {
-          console.log('ERRO', e);
+          console.log('ERRO', e)
           if (e?.code === 'EADDRNOTAVAIL') {
-            console.log('Address not available, retrying...');
+            console.log('Address not available, retrying...')
             setTimeout(() => {
-              server.close();
+              server.close()
               server.listen(10292, 'localhost', () => {
-                console.log('Socket listening on port localhost:10292');
-              });
-            }, 1000);
+                console.log('Socket listening on port localhost:10292')
+              })
+            }, 1000)
           }
-        });
+        })
 
         server?.listen(10292, 'localhost', () => {
-          console.log('Socket listening on port 10292');
-        });
+          console.log('Socket listening on port 10292')
+        })
 
         server?.on('connection', (connection) => {
-          console.log('connection', connection);
-
           connection.on('data', function (data) {
-            const convertedMessage = messageConverter(data);
+            const convertedMessage = messageConverter(data)
+
             switch (convertedMessage.type) {
-              case 'connectionId': {
-                connectedSockets[convertedMessage.host] = connection;
-                io.emit('connection', {
+              case 'connectionId':
+                {
+                  connectedSockets[convertedMessage.host] = connection
+                  console.log('CONECTADO TESTE')
+                  io.emit('CONNECTION', {
+                    host: convertedMessage.host,
+                    type: 'connection',
+                    message: 1
+                  })
+                }
+                console.log('DATA', messageConverter(data))
+              case 'nightMode': {
+                io.emit('NIGHT_MODE', {
                   host: convertedMessage.host,
-                  type: 'connection',
-                  message: 1,
-                });
+                  type: 'nightMode',
+                  message: 1
+                })
               }
-
-              //   case 'nightMode': {
-
-              //   }
             }
-          });
+          })
 
           connection.on('end', function () {
             Object.keys(connectedSockets).forEach((socket) => {
               if (connectedSockets[socket] === connection) {
-                console.log(`${socket} disconnected`);
-                io.emit('connection', {
+                console.log(`${socket} disconnected`)
+                io.emit('CONNECTION', {
                   from: socket,
                   type: 'connection',
-                  value: 0,
-                });
-                delete connectedSockets[socket];
+                  value: 0
+                })
+                delete connectedSockets[socket]
               }
-            });
-          });
-        });
+            })
+          })
+        })
       } else {
-        console.log('Socket.io already running');
+        console.log('Socket.io already running')
       }
 
-      break;
+      break
     }
 
     default: {
-      break;
+      break
     }
   }
 
-  res.end();
-};
+  res.end()
+}
 
-export default SocketHandler;
+export default SocketHandler
