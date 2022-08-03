5f7a0a4bc583583d2da918459aed4c3e692794bb
luucas-melo <lucasmelodos322@gmail.com>
2022-08-03 13:41:11 -0300

    Fix socket hook

--- central/src/hooks/useSocket.ts
+++ central/src/hooks/useSocket.ts
@@ -1,26 +1,27 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
-import { DefaultEventsMap } from 'socket.io/dist/typed-events';
-import { io, Socket } from 'socket.io-client';
+import { useEffect, useState } from 'react'
+import { DefaultEventsMap } from 'socket.io/dist/typed-events'
+import { io, Socket } from 'socket.io-client'
 
 export const useSocket = () => {
-  const socket = useRef<Socket<DefaultEventsMap, DefaultEventsMap> | null>(null);
+  const [socket, setSocket] = useState<Socket<
+    DefaultEventsMap,
+    DefaultEventsMap
+  > | null>(null)
 
-  const initSocker = useCallback(async () => {
-    await fetch('/api/socket');
-    socket.current = io();
-    console.log('socket.current', socket.current);
-  }, [socket]);
   useEffect(() => {
-    initSocker();
-    console.log('AAAA ');
+    const initSocket = async () => {
+      await fetch('/api/socket')
+      const socketIo = io()
+      setSocket(socketIo)
+    }
+
+    initSocket()
+
+    return () => {
+      socket?.disconnect?.()
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-  useEffect(() => {
-    console.log('socket', socket);
-  }, [socket]);
+  }, [])
 
-  //   socket?.current?.on('EVENTO', (data) => {
-  //     console.log('data', data);
-  //   });
-  return socket;
-};
+  return socket
+}
